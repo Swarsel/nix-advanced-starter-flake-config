aee59d79131f293d043611981da56f7788de2c71
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-01-02 18:28:04 +0100

    d

--- modules/nixos/setup.nix
+++ modules/nixos/setup.nix
@@ -1,8 +1,8 @@
 { lib, ...}:
 {
   options.custom.setup = {
-    isSwap = lib.mkEnabkeOption "use swap on the system";
-    isDisko = lib.mkEnabkeOption "use disko on the system";
+    isSwap = lib.mkEnableOption "use swap on the system";
+    isDisko = lib.mkEnableOption "use disko on the system";
     swapSize = lib.mkOption {
       type = lib.types.str;
       default = "8G";
